c00801f86c014ef1d0fb7818a04d73203c99fdae
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-29 22:45:58 -0600

    Graphql Gen

---
+++ src/generated/tmp-resolvers/Mutation.ts
@@ -0,0 +1,20 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { MutationResolvers } from "../graphqlgen";
+
+export const Mutation: MutationResolvers.Type = {
+  ...MutationResolvers.defaultResolvers,
+  createUser: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  createDraft: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  deletePost: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  publish: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  }
+};
---
+++ src/generated/tmp-resolvers/Query.ts
@@ -0,0 +1,17 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { QueryResolvers } from "../graphqlgen";
+
+export const Query: QueryResolvers.Type = {
+  ...QueryResolvers.defaultResolvers,
+  feed: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  drafts: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  post: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  }
+};
