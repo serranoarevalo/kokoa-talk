13f92ad64c18043025952420fc2eea86ee447e48
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-29 23:26:47 -0600

    Prisma + importSchema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import { importSchema } from "graphql-import";
 import { resolvers } from "./resolvers";
 import { prisma } from "../prisma/prisma-client";
 
 const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
+  typeDefs: importSchema("./src/schema.graphql"),
   resolvers,
   context: { prisma }
 } as any);
+
+server.express.use(logger("tiny"));
 
 server.start(() => console.log("Server is running on http://localhost:4000"));
